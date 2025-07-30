b388dfb09427e2bd1a1eb1561bc3901567c093c3
EulCau <lx_0318@mail.ustc.edu.cn>
2025-07-30 11:31:55 +0800

    Add toggle for running exhaustive_search

--- src/main.py
+++ src/main.py
@@ -7,19 +7,25 @@ def load_data(data_path="../data/", data_name="sk30Jij.npy", data_slice=0):
 	data = np.load(data_path+data_name, allow_pickle=True)
 	return data[data_slice]
 
-def main():
-	data_path = "../data/"
-	data_name = "sk30Jij.npy"
-	data_slice = 0
+def main(run_exhaustive_search):
 	methods = [
+		"exhaustive_search",
 		"simulated_annealing",
 	]
-	J = load_data(data_path, data_name, data_slice)
+	J = load_data()
+	best_S_with_no_change_J = np.array(
+		[-1,1,-1,-1,-1,-1,1,1,-1,-1,-1,1,-1,1,-1,-1,-1,1,1,-1,-1,1,1,-1,1,-1,-1,-1,-1,-1])
+	best_E_with_no_change_J = -21.6217
+	information = "Warning: The exhaustive search did not execute; "\
+				  "the results are based solely on the initially provided first set from the given sets."
 
 	for method_name in methods:
+		if (method_name == "exhaustive_search") and not run_exhaustive_search:
+			print(method_name, information, best_S_with_no_change_J, best_E_with_no_change_J, sep="\n", end="\n\n")
+			continue
 		try:
 			module = importlib.import_module(method_name)
-			best_S, best_E = module.compute_min_energies(J, data_slice)
+			best_S, best_E = module.compute_min_energies(J, seed=1)
 			print(method_name, best_S, best_E, sep="\n", end="\n\n")
 		except ImportError:
 			print(f"error: can't find {method_name}")
@@ -28,4 +34,4 @@ def main():
 
 
 if __name__ == "__main__":
-	main()
+	main(False)
