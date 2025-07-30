4d9a270a80eae734d68fd1db542760ec8dfae92d
EulCau <lx_0318@mail.ustc.edu.cn>
2025-07-30 10:03:14 +0800

    Modularization methods

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
+import importlib
+
 import numpy as np
 
-from simulated_annealing import simulated_annealing
 
 def load_data(data_path="../data/", data_name="sk30Jij.npy", data_slice=0):
 	data = np.load(data_path+data_name, allow_pickle=True)
@@ -10,9 +11,21 @@ def main():
 	data_path = "../data/"
 	data_name = "sk30Jij.npy"
 	data_slice = 0
+	methods = [
+		"simulated_annealing",
+	]
 	J = load_data(data_path, data_name, data_slice)
-	best_S, best_E = simulated_annealing(J, 1)
-	print(best_S, best_E)
+
+	for method_name in methods:
+		try:
+			module = importlib.import_module(method_name)
+			best_S, best_E = module.compute_min_energies(J, data_slice)
+			print(method_name, best_S, best_E, sep="\n", end="\n\n")
+		except ImportError:
+			print(f"error: can't find {method_name}")
+		except AttributeError:
+			print(f"error: can't find compute_min_energies for {method_name}")
+
 
 if __name__ == "__main__":
 	main()
--- src/simulated_annealing.py
+++ src/simulated_annealing.py
@@ -9,7 +9,7 @@ def generate_neighbor(S):
     S_new[i] *= -1  # flip one spin
     return S_new
 
-def simulated_annealing(J, seed=None, T_init=10.0, T_min=1e-3, alpha=0.98, max_iter=10000):
+def compute_min_energies(J, seed=None, T_init=10.0, T_min=1e-3, alpha=0.98, max_iter=10000):
     if seed is not None:
         np.random.seed(seed)
     n = J.shape[0]
